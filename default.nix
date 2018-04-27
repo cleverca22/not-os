76e0a74667613092c66fcc7994a59bbcf576e93f
Michael Bishop <cleverca22@gmail.com>
2018-04-27 01:13:27 -0300

    fix changing the arch for the latest nixos

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ let
     _file = ./default.nix;
     key = _file;
     config = {
-      nixpkgs.system = pkgs.lib.mkDefault system;
+      nixpkgs.localSystem = { inherit system; };
     };
   };
   baseModules = [
