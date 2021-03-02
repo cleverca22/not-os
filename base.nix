bbdbfe119c0ddcabb47eb19fec5d53de2ee85fe3
Jörg Thalheim <joerg@thalheim.io>
2021-03-02 12:35:55 +0000

    qemu: remove unsupported vlan0 argument

--- base.nix
+++ base.nix
@@ -107,9 +107,8 @@ with lib;
         -drive index=0,id=drive1,file=${config.system.build.squashfs},readonly,media=cdrom,format=raw,if=virtio \
         -kernel ${config.system.build.kernel}/bzImage -initrd ${config.system.build.initialRamdisk}/initrd -nographic \
         -append "console=ttyS0 ${toString config.boot.kernelParams} quiet panic=-1" -no-reboot \
-        -net nic,vlan=0,model=virtio \
-        -net user,vlan=0,net=10.0.2.0/24,host=10.0.2.2,dns=10.0.2.3,hostfwd=tcp::2222-:22 \
-        -net dump,vlan=0 \
+        -net nic,model=virtio \
+        -net user,net=10.0.2.0/24,host=10.0.2.2,dns=10.0.2.3,hostfwd=tcp::2222-:22 \
         -device virtio-rng-pci
     '';
 
