75ccd1e6a0359ac23b3c4f63770547cdc1929af4
Michael Bishop <cleverca22@gmail.com>
2023-10-27 16:07:21 -0300

    enable nix

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [ ./qemu.nix ];
-  not-os.nix = false;
+  not-os.nix = true;
   environment.systemPackages = [ pkgs.utillinux ];
   environment.etc = {
     "ssh/authorized_keys.d/root" = {
